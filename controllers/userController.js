4d820d6fd171d38556c84fd1698068374547f66b
Juben Navarro <juben.lnavarro@gmail.com>
2024-10-01 17:51:09 +0800

    password

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,15 +7,15 @@ const createToken = (_id) => {
 
 // login a user
 const loginUser = async (req, res) => {
-  const { email, password } = req.body
+  const { username, password } = req.body
 
   try {
-    const user = await User.login(email, password)
+    const user = await User.login(username, password)
 
     // create a token
     const token = createToken(user._id)
 
-    res.status(200).json({ email, token })
+    res.status(200).json({ username, token })
   } catch (error) {
     res.status(400).json({ error: error.message })
   }
@@ -23,15 +23,15 @@ const loginUser = async (req, res) => {
 
 // signup a user
 const signupUser = async (req, res) => {
-  const { email, password } = req.body
+  const { username, password } = req.body
 
   try {
-    const user = await User.signup(email, password)
+    const user = await User.signup(username, password)
 
     // create a token
     const token = createToken(user._id)
 
-    res.status(200).json({ email, token })
+    res.status(200).json({ username, token })
   } catch (error) {
     res.status(400).json({ error: error.message })
   }
